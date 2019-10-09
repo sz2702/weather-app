60b6f6b6f65a33c9f0a7be2a8f06d2200695b6a1
Stan Zheng <stan@Stans-MacBook-Pro.local>
2019-10-09 18:02:59 -0400

    removed unnecessary console.log

--- server.js
+++ server.js
@@ -18,20 +18,16 @@ app.post('/', function (req, res) {
   let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
 
   request(url, function (err, response, body) {
-  	console.log('City' + city);
-  	console.log(url); 
 
     if(err){
       res.render('index', {weather: null, error: 'Error, please try again'});
     } else {
       let weather = JSON.parse(body)
-      console.log(weather);
       if(weather.main == undefined){
         res.render('index', {weather: null, error: 'Error, please try again'});
       } else {
         let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
         res.render('index', {weather: weatherText, error: null});
-        console.log(weatherText);
       }
     }
   });
